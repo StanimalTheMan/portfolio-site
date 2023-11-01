88020f25e65167e587c7fd5de90315d7dcb33f32
Stan Choi <sjc591@nyu.edu>
2023-11-01 12:59:49 -0400

    Set up skeleton for components that are not part of pages and put in dummy description of a project for now

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,6 +2,16 @@ const ProjectsPage = () => {
   return (
     <>
       <h1>My Projects</h1>
+      <h2>Positive Vibes Only</h2>
+      <p>
+        <a href="https://github.com/StanimalTheMan/positive-vibes-only">
+          Positive Vibes Only src code
+        </a>
+        A full stack application with Java/SpringBoot, Maven, React, AWS RDS,
+        AWS Comprehend & Elastic Beanstalk, Docker, PostgreSQL, CI/CD that lets
+        users make a habit of tracking gratitude and action items in place of
+        negative thinking.
+      </p>
     </>
   )
 }
